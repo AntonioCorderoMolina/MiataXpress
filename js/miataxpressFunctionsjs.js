c592a8aa903c30a3ed18e0175c314558ebe0f562
Antonio Cordero Molina <acormol2001@g.educaand.es>
2023-12-13 11:55:17 +0100

    Modal Box for Copyright added - 13/12/23

--- js/miataxpressFunctionsjs.js
+++ js/miataxpressFunctionsjs.js
@@ -69,3 +69,31 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+/******** Modal Script form W3Schools *********/
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
